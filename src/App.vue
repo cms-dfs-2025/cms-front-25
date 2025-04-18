9855c7a78c6a948ab6bffc1b06cdd66807087bd6
Marianicko <1mar8nic9@gmail.com>
2025-04-18 21:09:05 +0300

    Set up the routes correctly

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <nav>
+      <!--
       <router-link to="/signup">Регистрация</router-link> |
+      -->
       <router-link to="/test">Тестовая (компонент TextEitor)</router-link> |
-      <router-link to="/auth/login">Регистрация с API</router-link>
+      <router-link to="/auth/login">Регистрация с API</router-link> |
+      <router-link to="/auth/reset-password">Сброс пароля</router-link> |
+      <router-link to="/auth/create-password">Новый пароль с API</router-link>
     </nav>
     <router-view />
   </div>
